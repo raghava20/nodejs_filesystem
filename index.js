a98163eda6f2ff10df9fbe8c0c05bd329801642f
raghava20 <raghavaanand22@gmail.com>
2021-12-26 18:24:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var fs = require("fs");
+var moment = require('moment'); // require
+var express = require("express")
+var cors = require("cors")
+// var index = require("./index.html")
+
+const app = express()
+
+app.use(cors()); // every request to be allowed in
+app.use(express.json());
+
+let result = []
+let final = []
+
+app.get("/", (req, res) => {
+
+    ''
+    res.send("Hi! Please use: <br><br> <i>/timestamp</i> endpoint for getting the current timestamp <br><br> <i>/files</i> endpoint for getting text files");
+})
+
+app.get("/timestamp", (req, res) => {
+
+    var timestamp = new Date().getTime()
+    console.log(timestamp)
+
+    var date = moment(timestamp).format("DD-MM-YYYY_h-mm-ss_A");
+    console.log(date)
+
+
+    const writeFile = async (timestamp, date) => {
+
+        await fs.writeFile(`./downloads/${date}.txt`, `The current timestamp is: ${timestamp.toString()}`, (err) => {
+            console.log("created")
+            result.push({
+                "timestamp": `The current timestamp is: ${timestamp.toString()}`,
+                "filename": `${date}.txt`,
+            })
+            res.send(result)
+
+        })
+
+    }
+    writeFile(timestamp, date)
+})
+
+
+app.get("/files", (req, res) => {
+    let getFiles = async () => {
+        await fs.readdir("./downloads", (err, files) => {
+            console.log(files)
+            final.push(files)
+            let result = [...final]
+            res.send(result)
+        })
+    }
+    getFiles();
+})
+
+app.listen(3000)
