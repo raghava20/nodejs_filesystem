fa4cf0a05559a363900846e62996aa5516d1bc7a
raghava20 <raghavaanand22@gmail.com>
2021-12-26 18:41:13 +0530

    port updated

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var fs = require("fs");
 var moment = require('moment'); // require
 var express = require("express")
 var cors = require("cors")
+var env = require("dotenv")
 
 const app = express()
-const PORT = 8000;
+const PORT = 8000 || process.env.PORT;
 
 app.use(cors()); // every request to be allowed in
 app.use(express.json());
